380235ecc42d76513be7342c252a0017cd3c978c
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-21 18:34:04 +0300

    webserver

--- webserver.py
+++ webserver.py
@@ -1,4 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from django.http import HttpResponse
+from yookassa.domain.notification import WebhookNotification
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -9,12 +12,36 @@ class handler(BaseHTTPRequestHandler):
         message = "Lobanova senior-pomidor!!!11"
         self.wfile.write(bytes(message, "utf8"))
 
-    def do_POST(self):
+    def do_POST(self, request):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         message = "POST response"
+        event_json = json.loads(request.body)
         self.wfile.write(bytes(message, "utf8"))
+        try:
+            print("notification_object")
+            notification_object = WebhookNotification(event_json)
+        except Exception:
+            message = "POST response error"
+    
+        payment = notification_object.object
+        message2 = payment
+        self.wfile.write(bytes(message2, "utf8"))
+
+
+# def my_webhook_handler(request):
+#     event_json = json.loads(request.body)
+#     return HttpResponse(status=200)
+
+# # Cоздайте объект класса уведомлений в зависимости от события
+# try:
+#     notification_object = WebhookNotification(event_json)
+# except Exception:
+#     # обработка ошибок
+
+# # Получите объекта платежа
+# payment = notification_object.object
 
 with HTTPServer(('', 443), handler) as server:
     server.serve_forever()
