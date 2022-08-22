80e8f13bd54d0ee31e525359c9c35ee402e213b8
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-22 10:24:07 +0300

    vs3

--- webserver.py
+++ webserver.py
@@ -12,42 +12,35 @@ class handler(BaseHTTPRequestHandler):
         message = "Lobanova senior-pomidor!!!11"
         self.wfile.write(bytes(message, "utf8"))
 
-    def do_POST(self, request):
+    def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         message = "POST response"
         event_json = json.loads(request.body)
         self.wfile.write(bytes(message, "utf8"))
-        try:
-            print("notification_object")
-            notification_object = WebhookNotification(event_json)
-        except Exception:
-            message = "POST response error"
+        # try:
+        #     print("notification_object")
+        #     notification_object = WebhookNotification(event_json)
+        # except Exception:
+        #     message = "POST response error"
     
-        payment = notification_object.object
-        message2 = payment
-        self.wfile.write(bytes(message2, "utf8"))
+        # payment = notification_object.object
+        # message2 = payment
+        # self.wfile.write(bytes(message2, "utf8"))
+        
+def my_webhook_handler(request):
+    event_json = json.loads(request.body)
+    return HttpResponse(status=200)
 
+# Cоздайте объект класса уведомлений в зависимости от события
+try:
+    notification_object = WebhookNotification(event_json)
+except Exception:
+    # обработка ошибок
 
-    # def my_webhook_handler(request):
-    #     self.send_header('Content-type','text/html')
-    #     self.end_headers()
-    #     event_json = json.loads(request.body)
-    #     message = "POST response"
-    #     self.wfile.write(bytes(message, "utf8"))
-    #             # Cоздайте объект класса уведомлений в зависимости от события
-    #     try:
-    #         print("notification_object")
-    #         notification_object = WebhookNotification(event_json)
-    #     except Exception:
-    #         print("notification_object error")
-    #     # обработка ошибок
-
-    #     # Получите объекта платежа
-    #     payment = notification_object.object
-
-    #     return HttpResponse(status=200)
+# Получите объекта платежа
+payment = notification_object.object
 
 #         # Cоздайте объект класса уведомлений в зависимости от события
 # try:
