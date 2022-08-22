20003ba951b6c93b29f2de4db45d14897edf9559
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-22 15:16:27 +0300

    vs3

--- webserver.py
+++ webserver.py
@@ -18,11 +18,18 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
         message = "POST response"
+        self.wfile.write(bytes(message, "utf8"))
         # event_json = json.loads(request.body)
         # body_unicode = request.body.decode('utf-8')
         # body = json.loads(body_unicode)
         # content = body['content']
-        self.wfile.write(bytes(message, "utf8"))
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            json = request.json
+            return json
+        else:
+            return 'Content-Type not supported!'
+        
 
 # @app.route('/post_json', methods=['POST'])
     def process_json():
