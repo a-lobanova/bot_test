10ee946f5b5bdb571caaca48235968adefc7aa49
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-22 15:37:41 +0300

    vs3

--- webserver.py
+++ webserver.py
@@ -75,5 +75,11 @@ def process_json():
 #     # Получите объекта платежа
 # payment = notification_object.object
 
-with HTTPServer(('', 443), handler) as server:
-    server.serve_forever()
+# with HTTPServer(('', 443), handler) as server:
+#     server.serve_forever()
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello World with flask"
+if __name__ == '__webserver__':
+    app.run(port=443,debug=True)
