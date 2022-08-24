9f78e7375ed8ab1ecd2000130bef95c3124a632a
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-24 15:42:48 +0300

    ws app Flask

--- webserver.py
+++ webserver.py
@@ -5,86 +5,48 @@ from django.http import HttpResponse
 from yookassa.domain.notification import WebhookNotification
 from flask import Flask, request
 
-# app = Flask(__name__)
+# class handler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/html')
+#         self.end_headers()
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
+#         message = "Lobanova senior-pomidor!!!11"
+#         self.wfile.write(bytes(message, "utf8"))
 
-        message = "Lobanova senior-pomidor!!!11"
-        self.wfile.write(bytes(message, "utf8"))
-
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        message = "POST response"
-        self.wfile.write(bytes(message, "utf8"))
-        # event_json = json.loads(request.body)
-        # body_unicode = request.body.decode('utf-8')
-        # body = json.loads(body_unicode)
-        # content = body['content']
-        content_type = request.headers.get('Content-Type')
-        try:    
-            print("try")
-            json = request.json
-            return json
-        except:
-            return print('Content-Type not supported!')
+#     def do_POST(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/html')
+#         self.end_headers()
+#         message = "POST response"
+#         self.wfile.write(bytes(message, "utf8"))
+#         content_type = request.headers.get('Content-Type')
+#         try:    
+#             print("try")
+#             json = request.json
+#             return json
+#         except:
+#             return print('Content-Type not supported!')
         
 
-# @app.route('/post_json', methods=['POST'])
-# def process_json():
-#     content_type = request.headers.get('Content-Type')
-#     if (content_type == 'application/json'):
-#         json = request.json
-#         return json
-#     else:
-#         return 'Content-Type not supported!'
-        # try:
-        #     print("notification_object")
-        #     notification_object = WebhookNotification(event_json)
-        # except Exception:
-        #     message = "POST response error"
-    
-        # payment = notification_object.object
-        # message2 = payment
-        # self.wfile.write(bytes(message2, "utf8"))
+# with HTTPServer(('', 443), handler) as server:
+#     server.serve_forever()
 
-# def my_webhook_handler(request):
-#     event_json = json.loads(request.body)
-#     return HttpResponse(status=200)
 
-# # Cоздайте объект класса уведомлений в зависимости от события
-# try:
-#     notification_object = WebhookNotification(event_json)
-# except Exception as e:
-#     # обработка ошибок
-#     print(repr(e))
-#     print("notification_object error")
-
-# # Получите объекта платежа
-# payment = notification_object.object
-
-#         # Cоздайте объект класса уведомлений в зависимости от события
-# try:
-#     print("notification_object")
-#     notification_object = WebhookNotification(event_json)
-# except Exception:
-#     print("notification_object error")
-#     # обработка ошибок
-
-#     # Получите объекта платежа
-# payment = notification_object.object
-
+app = Flask(__name__)
 
 # @app.route('/')
-# def index():
-#     return "Hello World with flask"
-# if __name__ == '__webserver__':
-#     app.run(port=443, debug=True)
+# def hello():
+#     return 'Hello!'
 
-with HTTPServer(('', 443), handler) as server:
-    server.serve_forever()
+@app.route('/post_json', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
+
+if __name__ == '__ws__':
+    app.run(port=443, debug=True)
