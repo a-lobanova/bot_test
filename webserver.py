53d1f2690fcd2af6d6724be1fe2c75c7ec81a193
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-08-31 14:54:33 +0300

    https

--- webserver.py
+++ webserver.py
@@ -33,7 +33,7 @@ print("HTTPServer")
 hostname = 'www.lobanova.ml:443'
 # PROTOCOL_TLS_CLIENT requires valid cert chain and hostname
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_verify_locations('/etc/letsencrypt/live/lobanova.ml/privkey.pem')
+context.load_verify_locations('/etc/letsencrypt/live/lobanova.ml/fullchain.pem')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
