d4817048f720b6fc18fa766da1de964dcd9bbf10
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-09-09 17:14:48 +0300

    main

--- main.py
+++ main.py
@@ -109,13 +109,22 @@ class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         print("do_POST")
+        # self.send_response(200)
+        # self.send_header('Content-type','application/json')
+        # self.end_headers()
+        # length = int(self.headers.get('content-length'))
+        # message = json.loads(self.rfile.read(length))
+        # # self.wfile.write(bytes(json.dumps(message), "utf8"))
+        # print (message) 
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
         self.send_response(200)
-        self.send_header('Content-type','application/json')
         self.end_headers()
-        length = int(self.headers.get('content-length'))
-        message = json.loads(self.rfile.read(length))
-        # self.wfile.write(bytes(json.dumps(message), "utf8"))
-        print (message) 
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
 
     # def do_POST(self, request):
     #     print("do_POST")
