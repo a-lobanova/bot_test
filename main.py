599388e85393138366c9146e1231ce11437cb0a0
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-09-11 23:45:12 +0300

    main

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ from multiprocessing import Process
 import json
 from django.http import HttpResponse
 from yookassa.domain.notification import WebhookNotification
-from io import BytesIO
+import tracemalloc
+
+tracemalloc.start()
 
 # from telebot import types
 
@@ -132,11 +134,12 @@ class handler(BaseHTTPRequestHandler):
                 db.set_update(order_id) 
                 user_id = db.get_user_id_through_order_id(order_id)
                 order_inform = "Заказ оплачен через ЮКасса!\n" + "Пользователь: " + db.get_nickname(user_id) + db.get_paid_order_through_order_id(order_id) 
+                print(order_inform)
                 # remove inline buttons
                 # print("db.get_message_id(order_id)", db.get_message_id(order_id))
-                await bot.edit_message_text(chat_id=adminId, message_id=db.get_message_id(order_id), text = "Получена оплата за заказ #" + order_id)
-                await bot.send_message(message.from_user.id, "Платеж принят!")
-                await bot.send_message(adminId, order_inform, reply_markup=nav.orderRedeemedMurkup(order_id))
+                # await bot.edit_message_text(chat_id=adminId, message_id=db.get_message_id(order_id), text = "Получена оплата за заказ #" + order_id)
+                # await bot.send_message(message.from_user.id, "Платеж принят!")
+                # await bot.send_message(adminId, order_inform, reply_markup=nav.orderRedeemedMurkup(order_id))
             # Уведомление об успешном платеже за Доставку 
             elif db.get_orderStatus(order_id) == "wait delivery payment":
                 db.set_orderStatus(order_id, "paidOrderDelivery")
@@ -144,9 +147,9 @@ class handler(BaseHTTPRequestHandler):
                 user_id = db.get_user_id_through_order_id(order_id)
                 order_inform = "Доставка оплачена через ЮКасса!\n" + "Пользователь: " + db.get_nickname(user_id) + db.get_delivery_paid_order_through_order_id(order_id)  
                     # remove inline buttons
-                await bot.edit_message_text(chat_id=adminId, message_id=db.get_message_id(order_id), text = "Получена оплата за ДОСТАВКУ заказа #" + order_id)
-                await bot.send_message(message.from_user.id, "Платеж за доставку принят!")
-                await bot.send_message(adminId, order_inform, reply_markup=nav.sentOrderMurkup(order_id))
+                # await bot.edit_message_text(chat_id=adminId, message_id=db.get_message_id(order_id), text = "Получена оплата за ДОСТАВКУ заказа #" + order_id)
+                # await bot.send_message(message.from_user.id, "Платеж за доставку принят!")
+                # await bot.send_message(adminId, order_inform, reply_markup=nav.sentOrderMurkup(order_id))
 
     def SendAsync(self):
         loop = asyncio.get_event_loop()
