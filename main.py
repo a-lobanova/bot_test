5d6134cbdbde50c2d5d2938f6701143bc183166d
a-lobanova <94009759+a-lobanova@users.noreply.github.com>
2022-09-14 16:36:50 +0300

    lobanova.net

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ class handler(BaseHTTPRequestHandler):
         if status == "payment.succeeded":
             # Уведомление об успешном платеже за заказ 
             if db.get_orderStatus(order_id) == "wait payment":
-                db.set_orderStatus(order_id, "complitedPayment")
+                # db.set_orderStatus(order_id, "complitedPayment")
                 db.set_update(order_id) 
                 user_id = db.get_user_id_through_order_id(order_id)
                 order_inform = "Заказ оплачен через ЮКасса!\n" + "Пользователь: " + db.get_nickname(user_id) + db.get_paid_order_through_order_id(order_id) 
@@ -134,7 +134,7 @@ class handler(BaseHTTPRequestHandler):
                 # print("db.get_message_id(order_id)", db.get_message_id(order_id))
                 asyncio.run(bot.edit_message_text(chat_id=adminId, message_id=db.get_message_id(order_id), 
                     text = "Получена оплата за заказ #" + order_id))
-                asyncio.run(bot.send_message(message.from_user.id, "Платеж принят!"))
+                asyncio.run{(bot.send_message(message.from_user.id, "Платеж принят!"))}
                 asyncio.run(bot.send_message(adminId, order_inform, reply_markup=nav.orderRedeemedMurkup(order_id)))
             # Уведомление об успешном платеже за Доставку 
             elif db.get_orderStatus(order_id) == "wait delivery payment":
